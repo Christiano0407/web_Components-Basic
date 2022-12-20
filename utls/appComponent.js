cdb1c53c689ef77296568065c16fa2259fcc7e8a
Christiano0407 <placebo0704@gmail.com>
2022-12-20 12:56:45 -0600

    new css

--- utls/appComponent.js
+++ utls/appComponent.js
@@ -1,1 +1,15 @@
 //** === ==== ======================================================= WEB COMPONENT ( LEGO ) ======================================================== === */
+
+class myComponent extends HTMLAreaElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+  }
+
+  render() {}
+
+  connectedCallback() {}
+}
+
+customElements.define(`my-element`, myComponent);
