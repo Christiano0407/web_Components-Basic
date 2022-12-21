da18f4eee6db58dfe1564d5df7ecfff3047534e0
Christiano0407 <placebo0704@gmail.com>
2022-12-21 09:36:38 -0600

    component-web-part02

--- project/component/card-component.js
+++ project/component/card-component.js
@@ -1,14 +1,42 @@
 //**TODO === === ========================================== Card Component ================================================ === ===  */
-class myComponent extends HTMLAreaElement {
+class myElement extends HTMLElement {
   constructor() {
     super();
 
     this.attachShadow({ mode: 'open' });
   }
 
-  render() {}
+  getTemplate() {
+    const template = document.createElement(`template`);
 
-  connectedCallback() {}
+    template.innerHTML = ` 
+    <main>
+        <section>
+            <figure>
+                <img>
+            </figure>
+        </section>
+        <section>
+            <div>
+                <h2>Hello World</h2>
+                <p></p>
+                <h3></h3>
+                <button></button>
+            </div>
+        </section>
+    </main>
+
+    `;
+    return template;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.render();
+  }
 }
 
-customElements.define(`my-productCard`, myComponent);
+customElements.define(`product-card`, myElement);
