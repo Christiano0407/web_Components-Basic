f750ce7415339edd4488ceeac3fd934f19c82f0f
Christiano0407 <placebo0704@gmail.com>
2022-12-22 12:04:38 -0600

    secret

---
+++ apiProject/secret/secret.js
@@ -0,0 +1,6 @@
+//** === API KEY */
+const API_KEY = `c66eb9e2b42b5d1d179fff7ac34ce71f`;
+//** === Token */
+const Token = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjZlYjllMmI0MmI1ZDFkMTc5ZmZmN2FjMzRjZTcxZiIsInN1YiI6IjYxODZlNGYyYTMxM2I4MDA4ZjQ1Y2NkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._SyV7z_TCNXZRmYDiihDspxJTkXT8LFU5MueX4MHDsg`;
+
+export { API_KEY, Token };
