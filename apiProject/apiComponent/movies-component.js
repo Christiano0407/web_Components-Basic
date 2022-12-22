9c9495fcce406743cf4ea577a5e0294afebf2627
Christiano0407 <placebo0704@gmail.com>
2022-12-22 12:29:10 -0600

    api card

--- apiProject/apiComponent/movies-component.js
+++ apiProject/apiComponent/movies-component.js
@@ -1,1 +1,3 @@
 //**todo === === ========================== Movie API REST Component =============================== === ===  */
+//** === API KEY */
+import { API_KEY } from './secrets/secrets';
--- apiProject/appAPIMovie.js
+++ apiProject/appAPIMovie.js
@@ -1,1 +1,5 @@
 //** === === ========================================== PRINCIPAL API REST WEB COMPONENT ================================================ === ===  */
+//** === <=  IMPORT  >= ===  */
+//*? === API KEY */
+//import { API_KEY } from './secrets/secrets';
+import './apiComponent/movies-component.js';
