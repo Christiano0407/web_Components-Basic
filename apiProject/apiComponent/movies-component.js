5095e436a801b7771fead865dfb1cd6492cfad1d
Christiano0407 <placebo0704@gmail.com>
2022-12-23 10:03:58 -0600

    previous movie

--- apiProject/apiComponent/movies-component.js
+++ apiProject/apiComponent/movies-component.js
@@ -1,31 +1,56 @@
 //**todo === === ========================== Movie API REST Component =============================== === ===  */
 //** === API KEY */
 import { API_KEY } from '../secrets/secrets.js';
-const API_POPULAR = `https://api.themoviedb.org/3/movie/popular/?page=1&api_key=${API_KEY}`;
 
 //** === Component */
 
 class myElement extends HTMLElement {
   constructor() {
     super();
-
     this.attachShadow({ mode: `open` });
   }
 
-  static get observedAttribute() {
-    return [`img`, `title`];
+  static get observedAttributes() {
+    return [`img`, `title`, 'movie'];
   }
 
   attributeChangedCallback(attr, oldVal, newVal) {
     if (attr === 'img' && oldVal !== newVal) {
       this.img = newVal;
     }
+
+    if (attr === 'movie' && oldVal !== newVal) {
+      this.movie = newVal;
+    }
   }
 
   getTemplate() {
     const template = document.createElement(`template`);
 
-    const getMovies = async () => {
+    template.innerHTML = `
+          <section class="movie_section">
+                <div class="movie_container">
+                    <img class="movie__container--img" src="${
+                      this.img
+                    }" alt="movie-DB">
+                </div>
+                <div>
+                    <img src="${this.movie}"
+                      class="moviePopular-img"
+                      alt="movies-Popular"
+                    />
+                </div>
+          </section>
+      ${this.getStyle()}
+      `;
+    return template;
+  }
+
+  /* getMovies() {
+    const templateNew = document.createElement(`template`);
+    const API_POPULAR = `https://api.themoviedb.org/3/movie/popular/?page=1&api_key=${API_KEY}`;
+    let popularMoviesNew = ``;
+    const getMovie = async () => {
       try {
         const response = await fetch(API_POPULAR, {
           method: `GET`,
@@ -36,35 +61,63 @@ class myElement extends HTMLElement {
 
         const dataPopular = await response.json();
 
-        dataPopular.forEach((movie) => {
-          template.innerHTML = `
-
-                  <section>
-                        <div>
-                            <img  src="https://image.tmdb.org/t/p/w500/${
-                              movie.poster_path
-                            }"  >
-                        </div>
-                  </section>
-    
-            ${this.getStyle()}
-            `;
+        dataPopular.forEach((movies) => {
+          popularMoviesNew += `
+              <div class="moviesPopular-container">
+                  <img
+                    src="https://image.tmdb.org/t/p/w500/${movies.poster_path}"
+                    class="moviePopular-img"
+                    alt="movies-Popular"
+                  />
+              </div>
+              `;
         });
-
-        return template;
+        templateNew.innerHTML = popularMoviesNew;
       } catch (error) {
         console.log('We have Error!');
       }
     };
-    getMovies();
-  }
+    getMovie();
+    return templateNew;
+  } */
 
   getStyle() {
     return `
     <style>
         :host {
+          --background-color: #ffffff;
+          --background-color-white: #f2f2f2;
+          --color-base: #333;
+          --primary-color:  #5a6cb2;
+          --color-primary: #264653;
+          --color-primary-plus: #1d3557;
+          --gradient-color: background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+          --gradient-color-plus: background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%);
+          --font-family-orbitron: 'Orbitron', sans-serif;
           width: 100%;
           margin: 0 auto;
+        }
+        .movie_section {
+          width: 100%;
+          padding: 10px;
+          margin: 10px auto;
+        }
+        .movie_container, .moviesPopular-container {
+          width: 100%;
+          padding: 10px;
+          margin: 10px auto;
+        }
+        .movie__container--img, .moviePopular-img {
+          width: 400px;
+          height: 200px;
+          border-radius: 20px;
+          object-fit: cover;
+        }
+
+        @media (max-width: 1080px) {
+          .movie__container {
+            width: auto;
+          }
         }
     </style>
     
@@ -73,6 +126,7 @@ class myElement extends HTMLElement {
 
   render() {
     this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    //this.shadowRoot.appendChild(this.getMovies().content.cloneNode(true));
   }
 
   connectedCallback() {
@@ -80,4 +134,4 @@ class myElement extends HTMLElement {
   }
 }
 
-customElements.define(`practice-card`, myElement);
+customElements.define(`practice-api`, myElement);
